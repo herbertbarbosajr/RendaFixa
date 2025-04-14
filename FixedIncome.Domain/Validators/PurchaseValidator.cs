d25951e564fc5fe76f9a856de9b0993e4b1c3eec
Herbert Barbosa <hebertbarbosajr@gmail.com>
2025-04-14 20:11:18 -0300

    [Feature] implementação do sonnarqube, rabbitmq, sqlserver no docker e utilizando seus volumes.

---
+++ FixedIncome.Domain/Validators/PurchaseValidator.cs
@@ -0,0 +1,39 @@
+﻿using FixedIncome.Domain.Entities;
+using FixedIncome.Domain.Interfaces;
+
+namespace FixedIncome.Domain.Validators
+{
+    public class PurchaseValidator : IPurchaseValidator
+    {
+        public void OrderValidator(CustomerAccount account, FixedIncomeProduct product, int quantity)
+        {
+            if (account == null)
+                throw new InvalidOperationException("Conta não encontrada.");
+
+            if (product == null)
+                throw new InvalidOperationException("Produto não encontrado.");
+
+            var totalPrice = product.UnitPrice * quantity;
+
+            if (account.Balance < totalPrice)
+                throw new InvalidOperationException("Saldo insuficiente.");
+
+            if (product.Stock < quantity)
+                throw new InvalidOperationException("Estoque insuficiente.");
+        }
+
+        public decimal ExecutePurchase(CustomerAccount account, FixedIncomeProduct product, int quantity)
+        {
+            if (product == null)
+                throw new InvalidOperationException("Produto não encontrado.");
+
+            if (account == null)
+                throw new InvalidOperationException("Conta não encontrada.");
+
+            var totalPrice = product.UnitPrice * quantity;
+            account.Balance -= totalPrice;
+            product.Stock -= quantity;
+            return totalPrice;
+        }
+    }
+}
